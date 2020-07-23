15e3e392e22f9eb55aa6071f47ffefdcce54dc9c
Dustin Crawford <dustin.crawford.2@gmail.com>
2020-07-23 15:26:55 -0400

    created register and login

--- books_app/models.py
+++ books_app/models.py
@@ -1,6 +1,36 @@
 from django.db import models
+import re
+import bcrypt
 
 # Create your models here.
+class UserManager(models.Manager):
+    def register_validator(self, postData):
+        errors = {}
+        if len(postData['first_name']) < 2:
+            errors['first_name'] = "First name must be at least 2 chars."
+        if len(postData['last_name']) < 2:
+            errors['last_name'] = "Last name must be at least 2 chars."
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        usedEmail = User.objects.filter(email=postData['email'])
+        if not EMAIL_REGEX.match(postData['email']):
+            errors['email'] = "The email you entered is not valid."
+        elif usedEmail:
+            errors['email'] = "That email is already registered."
+        if len(postData['password']) < 8:
+            errors['password'] = "Your password must be at least 8 chars."
+        elif postData['password'] != postData['confirm']:
+            errors['password'] = "Your passwords do not match"
+        return errors
+
+    def login_validator(self, postData):
+        errors = {}
+        emailExists = User.objects.filter(email=postData['email'])
+        if not emailExists:
+            errors['email'] = "That email is not registered."
+        elif not bcrypt.checkpw(postData['password'].encode(), emailExists[0].password.encode()):
+            errors['password'] = "Your credentials do not match."
+        return errors
+
 class User(models.Model):
     first_name = models.CharField(max_length=45)
     last_name = models.CharField(max_length=45)
@@ -9,6 +39,8 @@ class User(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
--- books_app/views.py
+++ books_app/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User, Book
+import bcrypt
+from django.contrib import messages
+
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def register(request):
+    errors = User.objects.register_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+    else:
+        firstName = request.POST['first_name']
+        lastName = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+
+        user = User.objects.create(first_name=firstName, last_name=lastName, email=email, password=pw_hash)
+        request.session['user_id'] = user.id
+        return redirect('/')
+
+def login(request):
+    errors = User.objects.login_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+    else:
+        email = request.POST['email']
+        password = request.POST['password']
+        user = User.objects.get(email=email)
+        request.session['user_id'] = user.id
+        return redirect('/')
